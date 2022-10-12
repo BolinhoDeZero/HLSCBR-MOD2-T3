3f9b1794baeb7a36c89a964f9e30aed665b13251
BolinhoDeZero <115438619+BolinhoDeZero@users.noreply.github.com>
2022-10-11 22:16:22 -0300

    Class 2: dinosaur HLSC

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Sprite
 
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
@@ -7,7 +8,7 @@ Y_POS = 310
 JUMP_VEL = 8.5
 Y_POS_DUCK = 340
 
-class Dinosaur:
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -31,12 +32,12 @@ class Dinosaur:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-        if user_input[pygame.K_DOWN] and not self.dino_jump:
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_jump = False
             self.dino_run = False
             self.dino_duck = True
 
-        elif not (self.dino_jump or user_input[pygame.K_DOWN]):
+        elif not self.dino_jump and not self.dino_duck:
             self.dino_jump = False
             self.dino_run = True
             self.dino_duck = False
@@ -69,6 +70,10 @@ class Dinosaur:
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
-        self.dino_rect.y = Y_POS
+        self.dino_rect.y = Y_POS_DUCK 
         self.step_index += 1
+        self.dino_duck = False
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
         
