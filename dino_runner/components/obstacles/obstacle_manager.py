251e761729e03f5f32d8cede21641822c276a813
BolinhoDeZero <115438619+BolinhoDeZero@users.noreply.github.com>
2022-10-13 22:29:01 -0300

    Class 3: Dinosaur

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,33 +1,31 @@
 import pygame
 import random
 
-from dino_runner.components.obstacles.cactus_and_bird import SmallCactus
-from dino_runner.components.obstacles.cactus_and_bird import LargeCactus
-from dino_runner.components.obstacles.cactus_and_bird import Bird
-from dino_runner.utils.constants import SMALL_CACTUS
-from dino_runner.utils.constants import LARGE_CACTUS
-from dino_runner.utils.constants import BIRD
+from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.bird import Bird
 
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
-
     def update(self, game):
-        if len(self.obstacles) == 0:
-            if random.randint(0,2) == 0:
-                self.obstacles.append(SmallCactus(SMALL_CACTUS))
-            elif random.randint(0,2) == 1:
-                self.obstacles.append(LargeCactus(LARGE_CACTUS))
-            elif random.randint(0,2) == 2:
-                self.obstacles.append(Bird(BIRD))
-
+        obstacle_type = [
+            Cactus(),
+            Bird(),
+        ]
+        if len(self.obstacles) == 0:            
+            self.obstacles.append(obstacle_type[random.randint(0,1)])
+            
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
                 break
+
+    def reset_obstacles(self):
+        self.obstacles = []
 
     def draw(self, screen):
         for obstacle in self.obstacles:
